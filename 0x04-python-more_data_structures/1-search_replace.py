e42b0b05edfcde90233aae7d6fdd0cfa2b217899
Giwa <giwatitilope899@gmail.com>
2022-12-07 17:45:22 +0000

    python

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
+earch_replace(my_list, search, replace):
 
+    if len(my_list) == 0:
+        return my_list
+
+    new_lst = [elem if elem != search else replace for elem in my_list]
+    return new_lst
